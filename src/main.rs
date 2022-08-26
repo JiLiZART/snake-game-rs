ffff394f3b3a7bf33c3e66c45b35f4834fecb9b1
Nikolay Kostyurin <jilizart@gmail.com>
2022-08-27 01:59:57 +0200

    feat: snake head

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
+mod snake;
+
+use crate::snake::{SNAKE_HEAD_COLOR, SnakeHead};
 use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .add_startup_system(setup_camera)
+        .add_startup_system(spawn_snake)
         .add_plugins(DefaultPlugins)
         .run();
+}
+
+fn setup_camera(mut commmands: Commands) {
+    commmands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn spawn_snake(mut commmands: Commands) {
+    commmands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(SnakeHead);
 }
---
+++ src/snake.rs
@@ -0,0 +1,6 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct SnakeHead;
+
+pub const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
